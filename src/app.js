dafb9a61442aacaf49715ed0ad69a143ac603b5e
Jeronimo luis Vera <jeronimo.l.vera@exxonmobil.com>
2022-12-27 10:20:23 -0300

    module 4

--- src/app.js
+++ src/app.js
@@ -7,8 +7,11 @@ app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: true }));
 
-const accountData = fs.readFileSync('src/json/accounts.json', { encoding: 'utf8' });
+const accountData = fs.readFileSync('src/json/accounts.json', {
+    encoding: 'utf8',
+});
 const accounts = JSON.parse(accountData);
 const userData = fs.readFileSync('src/json/users.json', { encoding: 'utf8' });
 const users = JSON.parse(userData);
@@ -16,6 +19,7 @@ const users = JSON.parse(userData);
 app.get('/', (req, res) => {
     res.render('index', { title: 'Account Summary', accounts: accounts });
 });
+
 app.get('/savings', (req, res) => {
     res.render('account', { account: accounts.savings });
 });
@@ -25,7 +29,41 @@ app.get('/checking', (req, res) => {
 app.get('/credit', (req, res) => {
     res.render('account', { account: accounts.credit });
 });
+
 app.get('/profile', (req, res) => res.render('profile', { user: users[0] }));
+
+app.get('/transfer', (req, res) => res.render('transfer'));
+app.post('/transfer', (req, res) => {
+    accounts[req.body.from].balance -= req.body.amount;
+    accounts[req.body.to].balance += parseInt(req.body.amount, 10);
+    let accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(
+        path.join(__dirname, 'json', 'accounts.json'),
+        accountsJSON,
+        'utf8'
+    );
+
+    res.render('transfer', { message: 'Transfer Completed' });
+});
+
+app.get('/payment', (req, res) =>
+    res.render('payment', { accounts: accounts.credit })
+);
+app.post('/payment', (req, res) => {
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount, 10);
+    let accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(
+        path.join(__dirname, 'json', 'accounts.json'),
+        accountsJSON,
+        'utf8'
+    );
+
+    res.render('transfer', {
+        message: 'Payment Successful',
+        account: accounts.credit,
+    });
+});
 
 app.listen(3000, () => {
     console.log('PS Project Running on Port 3000!');
