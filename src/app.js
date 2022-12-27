e543de6bcfcf916339c56c3e9d57e6304c1c6845
Jeronimo luis Vera <jeronimo.l.vera@exxonmobil.com>
2022-12-27 10:57:39 -0300

    module 5

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
-const { accounts, users, writeJSON } = require('./data');
+const { accounts, users } = require('./data');
+const accountRoutes = require('./routes/accounts.js');
+const servicesRoutes = require('./routes/services.js');
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
@@ -14,40 +16,10 @@ app.get('/', (req, res) => {
     res.render('index', { title: 'Account Summary', accounts: accounts });
 });
 
-app.get('/savings', (req, res) => {
-    res.render('account', { account: accounts.savings });
-});
-app.get('/checking', (req, res) => {
-    res.render('account', { account: accounts.checking });
-});
-app.get('/credit', (req, res) => {
-    res.render('account', { account: accounts.credit });
-});
-
 app.get('/profile', (req, res) => res.render('profile', { user: users[0] }));
 
-app.get('/transfer', (req, res) => res.render('transfer'));
-app.post('/transfer', (req, res) => {
-    accounts[req.body.from].balance -= req.body.amount;
-    accounts[req.body.to].balance += parseInt(req.body.amount, 10);
-    writeJSON();
-
-    res.render('transfer', { message: 'Transfer Completed' });
-});
-
-app.get('/payment', (req, res) =>
-    res.render('payment', { accounts: accounts.credit })
-);
-app.post('/payment', (req, res) => {
-    accounts.credit.balance -= req.body.amount;
-    accounts.credit.available += parseInt(req.body.amount, 10);
-    writeJSON();
-
-    res.render('payment', {
-        message: 'Payment Successful',
-        account: accounts.credit,
-    });
-});
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
 app.listen(3000, () => {
     console.log('PS Project Running on Port 3000!');
